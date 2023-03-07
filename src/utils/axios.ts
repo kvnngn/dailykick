2ee1dc3819db313158c204af655562641e48cea0
Kevin Nguyen <kevin.nguyen@vitamojo.com>
2023-03-07 00:09:16 +0000

    fix: format

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,43 +1,43 @@
 /* eslint-disable no-param-reassign */
-import axios from 'axios';
-import { API_URL, GLOBAL } from 'src/constants';
-import { isExpired } from './common';
+import axios from 'axios'
+import { API_URL, GLOBAL } from 'src/constants'
+import { isExpired } from './common'
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_PROXY,
   headers: {
-    'Content-Type': 'application/json'
-  }
-});
+    'Content-Type': 'application/json',
+  },
+})
 
 axiosInstance.interceptors.request.use((config) => {
   if (
     config.url === `${API_URL.PROFILE}/tokenlogin` ||
     config.url === `${API_URL.PROFILE}/token_refresh`
   ) {
-    return config;
+    return config
   }
   if (config.headers?.Authorization) {
-    return config;
+    return config
   }
-  const token = localStorage.getItem(GLOBAL.ACCESS_TOKEN);
+  const token = localStorage.getItem(GLOBAL.ACCESS_TOKEN)
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`
   }
-  return config;
-});
+  return config
+})
 
 axiosInstance.interceptors.response.use(
   (response) => response,
   async (err) => {
     if (isExpired(err)) {
-      localStorage.removeItem(GLOBAL.REFRESH_TOKEN_EXPIRED);
-      localStorage.removeItem(GLOBAL.REFRESH_TOKEN);
-      localStorage.removeItem(GLOBAL.ACCESS_TOKEN_EXPIRED);
-      localStorage.removeItem(GLOBAL.ACCESS_TOKEN);
+      localStorage.removeItem(GLOBAL.REFRESH_TOKEN_EXPIRED)
+      localStorage.removeItem(GLOBAL.REFRESH_TOKEN)
+      localStorage.removeItem(GLOBAL.ACCESS_TOKEN_EXPIRED)
+      localStorage.removeItem(GLOBAL.ACCESS_TOKEN)
     }
-    throw err;
-  }
-);
+    throw err
+  },
+)
 
-export default axiosInstance;
+export default axiosInstance
